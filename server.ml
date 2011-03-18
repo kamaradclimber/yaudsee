b4848262ad53273b5f59e9cf2a943728c3ce824e
Grégoire Seux <kamaradclimber@gmail.com>
2011-03-18 14:16:33 +0100

    reprise du code non parrallèle trouvé sur le manuel ocaml

--- server.ml
+++ server.ml
@@ -1,11 +1,29 @@
-open Unix;;
+let get_my_addr () =
+    (Unix.gethostbyname(Unix.gethostname())).Unix.h_addr_list.(0) ;;
 
-let me  = inet_addr_of_string "192.168.1.2" ;;
-let mySocket = ADDR_INET (me,2203);;
 
-let working _ out = 
-    output_string out "Hello Bob !"
+let main_server  serv_fun =
+    try
+        let port =  2203 in 
+        let my_address = get_my_addr() in
+        Printf.printf "ouverture sur %s:%i\n" (Unix.string_of_inet_addr
+        my_address ) port; flush stdout;
+       Unix.establish_server serv_fun (Unix.ADDR_INET(my_address, port))
+    with
+    Failure("int_of_string") -> Printf.eprintf "serv_up : bad port number\n"
 ;;
 
+let uppercase_service ic oc =
+    try while true do    
+        let s = input_line ic in 
+        let r = String.uppercase s 
+        in output_string oc (r^"\n") ; flush oc
+    done
+    with _ -> Printf.printf "End of text\n" ; flush stdout ; exit 0 ;;
 
-let server = establish_server working mySocket;;
+
+let go_uppercase_service () = 
+    Unix.handle_unix_error main_server uppercase_service ;;
+
+
+go_uppercase_service ();;
